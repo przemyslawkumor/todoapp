55cd75bc9249ec4723c1f8283237f3cc23d5958b
Przemysław <przemekkumor4@gmail.com>
2022-10-24 12:41:02 +0200

    Updates

---
+++ precache-manifest.ed8537678e5614b76bf77310b614e958.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c6a0ba8e0222595c773",
+    "url": "/todoapp/static/css/main.1cc52012.chunk.css"
+  },
+  {
+    "revision": "2c6a0ba8e0222595c773",
+    "url": "/todoapp/static/js/main.2c6a0ba8.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/todoapp/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "ca8789d6bb6dac684f74",
+    "url": "/todoapp/static/js/runtime~main.ca8789d6.js"
+  },
+  {
+    "revision": "6f681e2987118fcb5c0bbc2526b046de",
+    "url": "/todoapp/index.html"
+  }
+];
